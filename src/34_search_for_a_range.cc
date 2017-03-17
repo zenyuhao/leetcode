05932733acf34289bfeee63629ecc24a90c9fc3f
霜天 <shuangtian.zyh@taobao.com>
2017-03-17 14:55:35 +0800

    first commit

---
+++ src/34_search_for_a_range.cc
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+       int start = -1;
+       int end = -1; 
+       for(int i=0; i < nums.size(); ++i) {
+            if(nums[i] == target) {
+                if(start == -1) {
+                    start = i;
+                }
+                end = i;
+            }
+        }
+        int returns[] = { start, end };
+        return vector<int>(returns, returns+2); 
+    }
+};
